5020786abe838ab823dc1c8e698518e1aec2a234
Donovan Thach <thachd@oregonstate.edu>
2025-10-06 00:18:23 -0700

    Practicing vectors and learning how to make a triangle appear within the window.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,18 @@
 #include <GLFW//glfw3.h>
 
 #include <iostream>
+#include <cmath>
 
+int main() {
+    // Initiallize GLFW
+    glfwInit();
+
+    // Tell GLFW what version of OpenGL we are using
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Tell GLFW we are using the CORE profile
+    // Only modern functions are available
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Vertex Shader source code
     const char* vertexShaderSource = "#version 330 core\n"
@@ -18,17 +29,12 @@
     "{\n"
     "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
     "}\n\0";
-    
-int main() {
-    // Initiallize GLFW
-    glfwInit();
 
-    // Tell GLFW what version of OpenGL we are using
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // Tell GLFW we are using the CORE profile
-    // Only modern functions are available
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+    };
 
     // Create a GLFWwindow object of 800 by 600 pixels, named "Physics Engine"
     GLFWwindow* window = glfwCreateWindow(800, 600, "Physics Engine", NULL, NULL);
@@ -46,6 +52,38 @@ int main() {
     // Specify the viewport of OpenGL in the window
     glViewport(0, 0, 800, 800);
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     // Specify the color of the background
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     // Clean the back buffer and assign the new color to it
@@ -54,8 +92,19 @@ int main() {
     glfwSwapBuffers(window);
 
     while(!glfwWindowShouldClose(window)) { // Main while loop
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+
         glfwPollEvents(); // Take care of all GLFW events
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // Delete window before ending the program
     glfwDestroyWindow(window);
